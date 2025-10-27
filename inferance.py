5852d0aefd8dd7e34a4b4c46997f351d5c96542b
shahd <shahed.khawam@gmail.com>
2025-10-28 00:33:39 +0300

    Create inferance.py

---
+++ inferance.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+"""
+Raspberry Pi Live Image Inference
+
+Continuously captures image from Raspberry Pi Camera module and perform 
+inference using provided .eim model file. Outputs probabilities in console.
+
+Author: EdgeImpulse, Inc.
+Date: June 8, 2021
+Updated: August 9, 2023
+License: Apache-2.0 (apache.org/licenses/LICENSE-2.0)
+"""
+import leap
+import os, sys, time
+import cv2
+import numpy as np
+from picamera2 import Picamera2
+from edge_impulse_linux.runner import ImpulseRunner
+import unltraleap
+
+
+# Settings
+model_file = "modelfile.eim"            # Trained ML model from Edge Impulse
+draw_fps = True                         # Draw FPS on screen
+res_width = 96                          # Resolution of camera (width)
+res_height = 96                         # Resolution of camera (height)
+rotation = 0                            # Camera rotation (0, 90, 180, or 270)
+cam_format = "RGB888"                   # Color format
+img_width = 28                          # Resize width to this for inference
+img_height = 28                         # Resize height to this for inference
+
+# The ImpulseRunner module will attempt to load files relative to its location,
+# so we make it load files relative to this program instead
+dir_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(dir_path, model_file)
+
+# Load the model file
+runner = ImpulseRunner(model_path)
+class Canvas:
+    
+    def __init__(self):
+        self.name = "Python Gemini Visualiser"
+        self.screen_size = [500, 700]
+        self.hands_colour = (255, 255, 255)
+        self.font_colour = (0, 255, 44)
+        self.hands_format = "Skeleton"
+        self.output_image = np.zeros((self.screen_size[0], self.screen_size[1], 3), np.uint8)
+        self.tracking_mode = None
+
+    def set_tracking_mode(self, tracking_mode):
+        self.tracking_mode = tracking_mode
+
+    def toggle_hands_format(self):
+        self.hands_format = "Dots" if self.hands_format == "Skeleton" else "Skeleton"
+        print(f"Set hands format to {self.hands_format}")
+
+    def get_joint_position(self, bone):
+        if bone:
+            return int(bone.x + (self.screen_size[1] / 2)), int(bone.z + (self.screen_size[0] / 2))
+        else:
+            return None
+
+    def render_hands(self, event):
+        while True:
+        # Convert image to grayscale
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+        # Resize captured image
+        img_resize = cv2.resize(img, (img_width, img_height))
+        
+        # Convert image to 1D vector of floating point numbers
+        features = np.reshape(img_resize, (img_width * img_height)) / 255
+        
+        # Edge Impulse model expects features in list format
+        features = features.tolist()
+        
+        # Perform inference
+        res = None
+        try:
+            res = runner.classify(features)
+        except Exception as e:
+            print("ERROR: Could not perform inference")
+            print("Exception:", e)
+            
+        # Display predictions and timing data
+        print("Output:", res)
+        
+        # Display prediction on preview
+        if res is not None:
+            
+            # Find label with the highest probability
+            predictions = res['result']['classification']
+            max_label = ""
+            max_val = 0
+            for p in predictions:
+                if predictions[p] > max_val:
+                    max_val = predictions[p]
+                    max_label = p
+                    
+            # Draw predicted label on bottom of preview
+            cv2.putText(img,
+                        max_label,
+                        (0, res_height - 20),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1,
+                        (255, 255, 255))
+                        
+            # Draw predicted class's confidence score (probability)
+            cv2.putText(img,
+                        str(round(max_val, 2)),
+                        (0, res_height - 2),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1,
+                        (255, 255, 255))
+        
+        # Draw framerate on frame
+        if draw_fps:
+            cv2.putText(img, 
+                        "FPS: " + str(round(fps, 2)), 
+                        (0, 12),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1,
+                        (255, 255, 255))
+        
+        # Show the frame
+        cv2.imshow("Frame", img)
+        
+        # Calculate framerate
+        frame_time = (cv2.getTickCount() - timestamp) / cv2.getTickFrequency()
+        fps = 1 / frame_time
+        
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+        # Clear the previous image
+        self.output_image[:, :] = 0
+
+        cv2.putText(
+            self.output_image,
+            f"Tracking Mode: {_TRACKING_MODES[self.tracking_mode]}",
+            (10, self.screen_size[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            self.font_colour,
+            1,
+        )
+
+        if len(event.hands) == 0:
+            return
+
+        for i in range(0, len(event.hands)):
+            hand = event.hands[i]
+            for index_digit in range(0, 5):
+                digit = hand.digits[index_digit]
+                for index_bone in range(0, 4):
+                    bone = digit.bones[index_bone]
+                    if self.hands_format == "Dots":
+                        prev_joint = self.get_joint_position(bone.prev_joint)
+                        next_joint = self.get_joint_position(bone.next_joint)
+                        if prev_joint:
+                            cv2.circle(self.output_image, prev_joint, 2, self.hands_colour, -1)
+
+                        if next_joint:
+                            cv2.circle(self.output_image, next_joint, 2, self.hands_colour, -1)
+
+                    if self.hands_format == "Skeleton":
+                        wrist = self.get_joint_position(hand.arm.next_joint)
+                        elbow = self.get_joint_position(hand.arm.prev_joint)
+                        if wrist:
+                            cv2.circle(self.output_image, wrist, 3, self.hands_colour, -1)
+
+                        if elbow:
+                            cv2.circle(self.output_image, elbow, 3, self.hands_colour, -1)
+
+                        if wrist and elbow:
+                            cv2.line(self.output_image, wrist, elbow, self.hands_colour, 2)
+
+                        bone_start = self.get_joint_position(bone.prev_joint)
+                        bone_end = self.get_joint_position(bone.next_joint)
+
+                        if bone_start:
+                            cv2.circle(self.output_image, bone_start, 3, self.hands_colour, -1)
+
+                        if bone_end:
+                            cv2.circle(self.output_image, bone_end, 3, self.hands_colour, -1)
+
+                        if bone_start and bone_end:
+                            cv2.line(self.output_image, bone_start, bone_end, self.hands_colour, 2)
+
+                        if ((index_digit == 0) and (index_bone == 0)) or (
+                            (index_digit > 0) and (index_digit < 4) and (index_bone < 2)
+                        ):
+                            index_digit_next = index_digit + 1
+                            digit_next = hand.digits[index_digit_next]
+                            bone_next = digit_next.bones[index_bone]
+                            bone_next_start = self.get_joint_position(bone_next.prev_joint)
+                            if bone_start and bone_next_start:
+                                cv2.line(
+                                    self.output_image,
+                                    bone_start,
+                                    bone_next_start,
+                                    self.hands_colour,
+                                    2,
+                                )
+
+                        if index_bone == 0 and bone_start and wrist:
+                            cv2.line(self.output_image, bone_start, wrist, self.hands_colour, 2)
+
+
+class TrackingListener(leap.Listener):
+    def __init__(self, canvas):
+        self.canvas = canvas
+
+    def on_connection_event(self, event):
+        pass
+
+    def on_tracking_mode_event(self, event):
+        self.canvas.set_tracking_mode(event.current_tracking_mode)
+        print(f"Tracking mode changed to {_TRACKING_MODES[event.current_tracking_mode]}")
+
+    def on_device_event(self, event):
+        try:
+            with event.device.open():
+                info = event.device.get_info()
+        except leap.LeapCannotOpenDeviceError:
+            info = event.device.get_info()
+
+        print(f"Found device {info.serial}")
+
+    def on_tracking_event(self, event):
+        self.canvas.render_hands(event)
+
+# Initialize model
+try:
+
+    # Print model information
+    model_info = runner.init()
+    print("Model name:", model_info['project']['name'])
+    print("Model owner:", model_info['project']['owner'])
+    
+# Exit if we cannot initialize the model
+except Exception as e:
+    print("ERROR: Could not initialize model")
+    print("Exception:", e)
+    if (runner):
+            runner.stop()
+    sys.exit(1)
+    
+# Initial framerate value
+fps = 0
+
+
+# Clean up
+cv2.destroyAllWindows()
+def main():
+    canvas = Canvas()
+
+    print(canvas.name)
+    print("")
+    print("Press <key> in visualiser window to:")
+    print("  x: Exit")
+    print("  h: Select HMD tracking mode")
+    print("  s: Select ScreenTop tracking mode")
+    print("  d: Select Desktop tracking mode")
+    print("  f: Toggle hands format between Skeleton/Dots")
+
+    tracking_listener = TrackingListener(canvas)
+
+    connection = leap.Connection()
+    connection.add_listener(tracking_listener)
+
+    running = True
+
+    with connection.open():
+        connection.set_tracking_mode(leap.TrackingMode.Desktop)
+        canvas.set_tracking_mode(leap.TrackingMode.Desktop)
+
+        while running:
+            cv2.imshow(canvas.name, canvas.output_image)
+
+            key = cv2.waitKey(1)
+
+            if key == ord("x"):
+                break
+            elif key == ord("h"):
+                connection.set_tracking_mode(leap.TrackingMode.HMD)
+            elif key == ord("s"):
+                connection.set_tracking_mode(leap.TrackingMode.ScreenTop)
+            elif key == ord("d"):
+                connection.set_tracking_mode(leap.TrackingMode.Desktop)
+            elif key == ord("f"):
+                canvas.toggle_hands_format()
+
+
+if __name__ == "__main__":
+    main()
